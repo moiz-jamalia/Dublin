f2cdc4f07ef869cde105a521d09fff9ac3bba1bc
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2022-12-05 23:03:44 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes : Routes = [
+  { path : '', component : PortfolioComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,19 @@
+import {Component, Renderer2} from '@angular/core';
+import {DomSanitizer} from "@angular/platform-browser";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent {
+
+  constructor(private router : Router, private renderer : Renderer2, private sanitizer : DomSanitizer) {
+
+  }
+
+  turnLogo(nb : number) {
+    this.renderer.setStyle(document.getElementById('logo-circle'), 'transform', 'rotate(' + nb * 360 + 'deg)')
+  }
+}
