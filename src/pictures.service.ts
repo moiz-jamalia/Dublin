af007e8f42a84614ee7eb8b3ebf40c70e2b7260b
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2022-12-12 23:51:48 +0100

    creating gallery view

---
+++ src/pictures.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Pictures } from "./app/pictures.model";
+
+@Injectable({
+  providedIn : 'root'
+})
+export class PicturesService {
+  private _pictures : Pictures[] = [
+    new Pictures(
+      'assets/Img/gallery/Sea_original.png',
+      'assets/Img/gallery/Sea_edited.png',
+      'Sea'
+    ),
+    new Pictures(
+      'assets/Img/gallery/Sea_original.png',
+      'assets/Img/gallery/Sea_edited.png',
+      'Sea2'
+    )
+  ];
+
+  getPictures() {
+    return [...this._pictures];
+  }
+
+  constructor() { }
+}
